3a6e7baa34926ff43d6f33962c63d5e83ba1d81d
Mak-43 <letsmailmak@gmail.com>
2022-05-23 08:43:30 +0600

    summary

--- src/Components/Home/Summary.js
+++ src/Components/Home/Summary.js
@@ -3,7 +3,41 @@ import React from 'react';
 const Summary = () => {
     return (
         <div>
-            Summary 
+            <div> <h2>Millions Business Trust Us </h2>
+                <h4>Try to Understand Users Expectation</h4>
+                <div></div></div>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1'>
+                <div>
+                    <i class="fa-solid fa-flag"></i>
+                    {/* <span class="countdown font-mono text-6xl">
+                        <span style="--value:72;"></span>   
+                    </span> */}
+                    <p>Countries</p>
+                </div>
+                <div>
+                <i class="fa-solid fa-clipboard-list"></i>
+                    {/* <span class="countdown font-mono text-6xl">
+                        <span style="--value:72;"></span>   
+                    </span> */}
+                    <p>Completed Projects</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-people-group"></i>
+                    {/* <span class="countdown font-mono text-6xl">
+                        <span style="--value:72;"></span>   
+                    </span> */}
+                    <p>Happy Clients</p>
+                </div>
+                <div>
+                    <i class="fa-solid fa-thumbs-up"></i>
+                    {/* <span class="countdown font-mono text-6xl">
+                        <span style="--value:72;"></span>   
+                    </span> */}
+                    <p>Feedbacks</p>
+                </div>
+            </div>
+
+
         </div>
     );
 };
